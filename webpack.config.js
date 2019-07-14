524f5a681e8e194d9698536ee473616357b2084f
neyron <andrey163783@gmail.com>
2019-07-14 16:01:07 +0400

    init

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+const {
+  NODE_ENV = 'production',
+} = process.env;
+module.exports = {
+  entry: './src/index.ts',
+  mode: NODE_ENV,
+  target: 'node',
+  externals: [ nodeExternals() ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'ts-loader',
+        ]
+      }
+    ]
+  }
+}
